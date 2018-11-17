894e0ae834185deda26095ccef5c32dcb478aad3
Raphael <raphaelz@users.noreply.github.com>
2018-11-16 22:55:34 -0500

    Add cat animation

--- kelpie/App.js
+++ kelpie/App.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.images = [
+        require('./assets/pet-1.png'),
+        require('./assets/pet-2.png'),
+        require('./assets/pet-3.png'),
+        require('./assets/pet-4.png'),
+        require('./assets/pet-5.png'),
+        require('./assets/pet-6.png'),
+        require('./assets/pet-7.png'),
+        require('./assets/pet-8.png'),
+        require('./assets/pet-9.png'),
+      ];
+    this.next = this.next.bind(this);
+    this.state = {index: 0};
+}
+
+componentDidMount() {
+  this.next();
+}
+
+next() {
+  setTimeout(() => {
+      this.setState({index: (this.state.index+1)%3});
+      this.next();
+  }, 200);
+}
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+                    <Image
+              source={this.images[this.state.index]}
+              style={styles.image}
+            />
       </View>
     );
   }
@@ -18,4 +49,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 300, 
+    height: 300,
+    justifyContent: 'center'
+  }
 });
