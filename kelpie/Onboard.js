4a14c2d144cefed4a8286102792440cf675f2cf3
Aleks Sobieraj <aleks@unoapp.com>
2018-11-17 16:27:50 -0500

    Egg and input on a background

--- kelpie/Onboard.js
+++ kelpie/Onboard.js
@@ -40,26 +40,82 @@ class OnboardScreen extends React.Component {
     const {error} = this.state
     const borderColor = error ? 'red': 'grey'
     return (
-      <View style={{ flex: 2, alignItems: "center", justifyContent: "center" }}>
-        <Image source={require('./assets/onboard-background.png')} resizeMode='cover' 
-        />
-        <Image
-          source={require('./assets/egg.png')}
-          style={{ top:-5 , width: 200, resizeMode: 'contain', marginLeft: 10, height:200 }}
-        />
-      
-        <TextInput
-          style={{height:40, borderColor, borderWidth:1, width:140, marginBottom:8, paddingHorizontal: 2, fontSize: 16}}
-          value={this.state.hatchCode}
-          onChangeText={text => {
-            this.setState({ hatchCode: text });
-          }}
-        />
-        {/* <Text>Onboard information</Text> */}
-        <Button
-          title="Go To Hatchery"
-          onPress={this.handleOnPress}
-        />
+      <View style={{ 
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: 'red'
+      }}>
+        <View style={{ 
+          zIndex: 1,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}>
+          <Image 
+            source={require('./assets/onboard-background.png')}
+            style={{flex:1, height: undefined, width: undefined}}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={{ 
+          zIndex: 2,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}>
+          <View
+            style={{
+              width: "100%",
+              height: "100%",
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "flex-end"
+            }}
+          >
+            <TextInput
+              style={{
+                height:40,
+                borderColor,
+                borderWidth:1,
+                width:140,
+                marginBottom:8,
+                paddingHorizontal: 2,
+                fontSize: 16
+              }}
+              value={this.state.hatchCode}
+              onChangeText={text => {
+                this.setState({ hatchCode: text });
+              }}
+            />
+            {/* <Text>Onboard information</Text> */}
+            <Button
+              title="Go To Hatchery"
+              onPress={this.handleOnPress}
+              style={{
+                marginBottom: "3%"
+              }}
+            />
+            <Image
+              source={require('./assets/egg.png')}
+              style={{
+                maxWidth: "50%",
+                maxHeight: "40%",
+                marginBottom: "20%"
+              }}
+              resizeMode="contain"
+            />
+          </View>
+        </View>
       </View>
     );
   }
